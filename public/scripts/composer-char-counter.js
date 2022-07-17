1b58afde338f33d281a0c9c085ea0b014237110b
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-07-17 18:51:40 -0300

    update

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,26 @@
+
 $(document).ready(function() {
   // --- our code goes here ---
+  const myTextArea = document.getElementById('tweet-text');
+  const remainingCharsText = document.getElementById('tweet-counter');
+  const maxChars = 140;
 
+  //eventlistener for input
+  myTextArea.addEventListener('input', () => {
+    const remaining = maxChars - myTextArea.value.length;
+    //if the remaining chars is less than 10%, color the text red
+    const color = remaining < maxChars * 0.1 ? 'red' : null;
+    remainingCharsText.textContent = `${remaining}`;
+    remainingCharsText.style.color = color;
+  });
 
+  $("#btn").on('click', function() {
+    console.log(this); //The this keyword is a reference to the button
+  });
+  
+  $("#btn").on('click', () => {
+    console.log(this); //The this keyword here refers to something else!
+  });
 });
 
-// reference for character counter: https://www.youtube.com/watch?v=X-LVkU95jLU
+
