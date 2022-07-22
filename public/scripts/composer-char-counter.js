e6682b6a52ffc1afb44bee38b501197c80b38479
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-07-21 23:12:18 -0300

    complete tweet layout and hover functionality

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,26 +1,37 @@
 
 $(document).ready(function() {
-  // --- our code goes here ---
+  console.log('Document ready');
+
   const myTextArea = document.getElementById('tweet-text');
   const remainingCharsText = document.getElementById('tweet-counter');
   const maxChars = 140;
 
+  const updateCountdown = function() {
+    let remaining = 140 - jQuery('#tweet-text').val().length;
+    jQuery('#tweet-counter').text(remaining);
+  };
+
+  jQuery(document).ready(function($) {
+    updateCountdown();
+    $('#tweet-text').change(updateCountdown);
+    $('#tweet-text').keyup(updateCountdown);
+  });
+
   //eventlistener for input
   myTextArea.addEventListener('input', () => {
     const remaining = maxChars - myTextArea.value.length;
-    //if the remaining chars is less than 10%, color the text red
-    const color = remaining < maxChars * 0.1 ? 'red' : null;
+    //if the remaining chars is less than 0, color the text red
+    const color = remaining < 0 ? 'red' : null;
     remainingCharsText.textContent = `${remaining}`;
     remainingCharsText.style.color = color;
   });
 
-  $("#btn").on('click', function() {
-    console.log(this); //The this keyword is a reference to the button
-  });
-  
-  $("#btn").on('click', () => {
-    console.log(this); //The this keyword here refers to something else!
-  });
 });
 
 
+/*
+references:
+https://www.youtube.com/watch?v=X-LVkU95jLU
+https://stackoverflow.com/questions/2136647/character-countdown-like-on-twitter
+https://stackoverflow.com/questions/2001366/how-can-i-change-the-text-color-with-jquery#:~:text=Place%20the%20following%20in%20your,color'%2C%20'red')%3B
+*/
